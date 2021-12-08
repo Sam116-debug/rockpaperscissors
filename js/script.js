aee9bafb62694329c73babe815a06b5fa5e85d6c
Glitch (samproject) <none>
2021-12-08 23:21:12 +0000

    ⚾️☔️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 
+let computerChoice =  Math.ceil(Math.random()*3); 
+
+//output of 1, 2, or 3
+
+ $(".computerChoice").text(computerChoice);
+
+
+
+$(".play").click(function(){
+  
+let userchoice = $(".input").val();
+  $(".userchoice").text(userchoice);
+               
+               
+               
+               
+               
+})
+/*condtional statemnt for computer choice 
+
+if (computerChoice === 1) {
+computerChoice = "rock";
+
+$(".computerChoice").text(computerChoice);
+} 
+else if (computer Choice === 2)
+computerChoice = "paper";
+
+else if (computerChoice === 3)
+computerChoice = "scissors";
